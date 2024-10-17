a477a1d08c58b992a5904e4d6da6690e45470206
bobneuj-jeu <unternehmen02191@gmail.com>
2024-10-17 10:55:58 +0900

    DELETE:: 불필요 의존성 제거

--- server.js
+++ server.js
@@ -1,45 +1,49 @@
-const express = require('express'); // express 모듈 가져오기
-const session = require('express-session'); // 세션 관리 모듈 가져오기
-const bodyParser = require('body-parser'); // 요청 본문 파싱 모듈 가져오기
-const usersRoutes = require('./routes/usersR'); // 인증 라우트 가져오기
-const mealRoutes = require('./routes/mealR'); // 식단 라우트 가져오기
-const fridgeRoutes = require('./routes/fridgeR'); // 냉장고 라우트 가져오기
+const express = require('express');
+const session = require('express-session');
+const usersRoutes = require('./routes/usersR');
+const mealRoutes = require('./routes/mealR');
+const fridgeRoutes = require('./routes/fridgeR');
+const successRoutes = require('./routes/successR');
 const requestLogger = require('./middleware/requestLogger');
 const authMiddleware = require('./middleware/auth');
 const errorMiddleware = require('./middleware/error');
-const successRoutes = require('./routes/successR');
 
-const path = require('path'); // 경로 처리 모듈 가져오기
+const path = require('path');
 const cors = require('cors');
 
-const app = express();
-app.use(cors()); // 모든 요청에 대해 CORS 허용
-const PORT = process.env.PORT || 3000; // 포트 설정
-
-app.use(bodyParser.json()); // JSON 요청 본문 파싱
-app.use(bodyParser.urlencoded({ extended: true })); // URL 인코딩된 요청 본문 파싱
 require('dotenv').config();
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// 미들웨어 설정
+app.use(requestLogger); // 모든 요청에 대해 로깅
+app.use(cors()); // 필요한 경우에만 CORS 허용
+app.use(express.json()); // JSON 요청 본문 파싱
+app.use(express.urlencoded({ extended: true })); // URL 인코딩된 요청 본문 파싱
+
 app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false, // 세션이 수정되지 않은 경우 다시 저장하지 않도록 설정
-    saveUninitialized: true, // 초기화되지 않은 세션을 저장하도록 설정
+    secret: process.env.SESSION_SECRET || 'defaultSecret', // 환경 변수 설정
+    resave: false,
+    saveUninitialized: false, // 필요하지 않은 세션을 저장하지 않도록 설정
 }));
+
+// 정적 파일 제공
 app.use(express.static(path.join(__dirname, 'public')));
 
 // 라우트 설정
-app.use('/api/users', usersRoutes); // 인증 관련 라우트
-app.use('/api/meals', mealRoutes); // 식단 관련 라우트
-app.use('/api/fridge', fridgeRoutes); // 냉장고 관련 라우트
-app.use('/api/meals', successRoutes); // 성공률
+app.use('/api/users', usersRoutes);
+app.use('/api/meals', mealRoutes);
+app.use('/api/fridge', fridgeRoutes);
+app.use('/api/success', successRoutes); // 경로 수정
 
-app.use(requestLogger);
-
-// 인증이 필요한 라우트에 대해 인증 미들웨어 사용
+// 인증이 필요한 라우트 설정
 app.use('/api/protected', authMiddleware);
-app.use(errorMiddleware); // 오류 처리 미들웨어 사용
+
+// 오류 처리 미들웨어
+app.use(errorMiddleware);
 
 // 서버 시작
-app.listen(process.env.PORT, () => {
-    console.log(`서버가 ${process.env.PORT}에서 실행 중입니다.`);
+app.listen(PORT, () => {
+    console.log(`서버가 ${PORT}에서 실행 중입니다.`);
 });
