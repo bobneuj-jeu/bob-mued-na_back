c72ce31011578866cca747308f192bfeb6b667c4
bobneuj-jeu <unternehmen02191@gmail.com>
2024-10-31 01:56:27 +0900

    FIX:: package 수정

--- punycode.js
+++
@@ -1,9 +0,0 @@
-const punycode = require('punycode/');
-
-// 예시: Punycode 인코딩
-const encoded = punycode.encode('안녕하세요');
-console.log(encoded);
-
-// 예시: Punycode 디코딩
-const decoded = punycode.decode(encoded);
-console.log(decoded);
