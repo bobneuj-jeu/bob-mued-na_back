3858f997ef423b90e614bc35c81a2e8ec1e28210
Rohyoohyun <unternehmen02191@gmail.com>
2024-11-12 21:32:03 +0900

    FIX:: 라우터 수정

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controller/userController');
 
+router.post('/signup', userController.signup);
+router.post('/login', userController.login);
+
 router.get('/info/:username', userController.getUserInfo);
 router.put('/change-password', userController.changePassword);
 router.put('/update-diseases', userController.updateUserDiseases);
