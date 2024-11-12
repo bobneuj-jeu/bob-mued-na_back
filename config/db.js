b69448ac790420d4befe109f093b5122bdcaa85a
Rohyoohyun <unternehmen02191@gmail.com>
2024-11-12 21:57:11 +0900

    FIX:: 포트번호 수정

--- config/db.js
+++ config/db.js
@@ -10,6 +10,17 @@ const pool = mariadb.createPool({
   connectionLimit: 5
 });
 
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mariadb',
+  pool: {
+    max: 5, // 최대 연결 수
+    min: 0,
+    acquire: 10000, // 연결을 시도하는 최대 시간
+    idle: 10000  // 연결이 사용되지 않을 때 풀로 돌아가기까지의 시간
+  }
+});
+
 pool.getConnection()
   .then(conn => {
     console.log('Database 연결 성공');
